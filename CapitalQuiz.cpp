281fa262d00d2d933f27a8b1d6183bd07043af5f
JoelSuh07 <joel.suh07@gmail.com>
2025-11-22 13:00:08 -0800

    Implement code changes to enhance functionality and improve performance

--- CapitalQuiz.cpp
+++ CapitalQuiz.cpp
@@ -1,2 +1,35 @@
 #include <map>
-#include 
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    string stateName, capitalCity;
+    ifstream file;
+
+    map<string, string> stateToCapital;
+    file.open("StateCapitals.txt");
+
+    if (!file.is_open()){
+        cout << "Error opening file";
+        return 1;
+    }
+    else{
+        while (file >> stateName){
+
+            getline(cin, stateName, ',');
+            stateToCapital[stateName] = capitalCity;
+        }
+        file.close();
+    }
+
+    cout << "Enter a State: ";
+    cin >> stateName;
+    if (stateToCapital.find(stateName) == stateToCapital.end()) {
+        cout << "State not found." << endl;
+        return 1;
+    }
+    cout << "The capital of " << stateName << " is " << stateToCapital[stateName] << "." << endl;
+    return 0;
+}
