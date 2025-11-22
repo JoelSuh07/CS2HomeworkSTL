714d54c09c7bb7ddfe278d7c6aed72d834c47b81
JoelSuh07 <joel.suh07@gmail.com>
2025-11-22 11:33:39 -0800

    Refactor UniqueWords.cpp to simplify file handling and remove unused normalization function

--- UniqueWords.cpp
+++ UniqueWords.cpp
@@ -2,51 +2,35 @@
 #include <iostream>
 #include <set>
 #include <string>
-#include <algorithm>
-#include <cctype>
-
 using namespace std;
 
-// Normalize a token: remove punctuation and convert to lowercase
-static inline void normalize_token(string &s) {
-    s.erase(remove_if(s.begin(), s.end(), [](unsigned char c){
-        return ispunct(c);
-    }), s.end());
-    transform(s.begin(), s.end(), s.begin(), [](unsigned char c){
-        return tolower(c);
-    });
-}
 
-int main(int argc, char* argv[]) {
-    string filename = (argc > 1) ? argv[1] : "LineFile.txt";
+ifstream file;
 
-    ifstream file(filename);
-    if (!file) {
-        cerr << "Failed to open file: " << filename << '\n';
-        return 1;
-    }
+int main(){
 
-    set<string> words;
     string line;
-    while (getline(file, line)) {
-        size_t i = 0;
-        while (i < line.size()) {
-            // skip non-alphanumeric characters
-            while (i < line.size() && !isalnum((unsigned char)line[i])) ++i;
-            size_t j = i;
-            while (j < line.size() && isalnum((unsigned char)line[j])) ++j;
-            if (j > i) {
-                string w = line.substr(i, j - i);
-                normalize_token(w);
-                if (!w.empty()) words.insert(w);
-            }
-            i = j;
+    ifstream file;
+
+    file.open("LineFile.txt");
+    //stores unique words
+    set<string> words;
+    if (file.is_open())
+    {
+        //Read the first line
+        while (getline(file,line))
+        {
+            //cout << line << '\n';
+            words.insert(line);
         }
+        file.close();
     }
 
-    file.close();
-
-    for (const auto &w : words) cout << w << '\n';
+    set<string>::iterator it;
+    for (it = words.begin();it != words.end(); it++)
+    {
+        cout << *it << endl; 
+    }
 
     return 0;
 }
