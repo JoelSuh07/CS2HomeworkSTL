2f6c9ae09213af5af74b2ab1787a05837560608b
JoelSuh07 <joel.suh07@gmail.com>
2025-11-22 11:32:48 -0800

    Refactor code structure for improved readability and maintainability

--- UniqueWords.cpp
+++ UniqueWords.cpp
@@ -2,35 +2,51 @@
 #include <iostream>
 #include <set>
 #include <string>
+#include <algorithm>
+#include <cctype>
+
 using namespace std;
 
+// Normalize a token: remove punctuation and convert to lowercase
+static inline void normalize_token(string &s) {
+    s.erase(remove_if(s.begin(), s.end(), [](unsigned char c){
+        return ispunct(c);
+    }), s.end());
+    transform(s.begin(), s.end(), s.begin(), [](unsigned char c){
+        return tolower(c);
+    });
+}
 
-ifstream file;
+int main(int argc, char* argv[]) {
+    string filename = (argc > 1) ? argv[1] : "LineFile.txt";
 
-int main(){
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Failed to open file: " << filename << '\n';
+        return 1;
+    }
 
-    string line;
-    ifstream file;
-
-    file.open("LineFile.txt");
-    //stores unique words
     set<string> words;
-    if (file.is_open())
-    {
-        //Read the first line
-        while (getline(file,line))
-        {
-            //cout << line << '\n';
-            words.insert(line);
+    string line;
+    while (getline(file, line)) {
+        size_t i = 0;
+        while (i < line.size()) {
+            // skip non-alphanumeric characters
+            while (i < line.size() && !isalnum((unsigned char)line[i])) ++i;
+            size_t j = i;
+            while (j < line.size() && isalnum((unsigned char)line[j])) ++j;
+            if (j > i) {
+                string w = line.substr(i, j - i);
+                normalize_token(w);
+                if (!w.empty()) words.insert(w);
+            }
+            i = j;
         }
-        file.close();
     }
 
-    set<string>::iterator it;
-    for (it = words.begin();it != words.end(); it++)
-    {
-        cout << *it << endl; 
-    }
+    file.close();
+
+    for (const auto &w : words) cout << w << '\n';
 
     return 0;
 }
